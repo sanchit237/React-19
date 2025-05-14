5e2fd80eef2e57dff7b77fcf4458fa573ebde228
sanchit237 <sanchitnemlekar8@gmail.com>
2025-05-14 09:02:40 +0530

    [Feat] Navigation and outlet implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import './categories.styles.scss';
 import { Route, Routes } from 'react-router-dom';
 
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+    </div>
+  );
+}
+
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   ); 
 }
